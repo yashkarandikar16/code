92d1af7acee9855d4b97ed617351c289883192a6
yash <yashkarandikar7@gmail.com>
2025-09-02 23:30:33 +0530

    Add files via upload

---
+++ index.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int age =20;
+    float pi =3.14;
+    char grade = 'A';
+
+
+    printf("hello students\n");
+    printf("Age = %d\n" , age);
+    printf("pi = %d\n", pi);
+    printf("pi = %f\n", pi);
+    printf("grade = %f\n , grade");
+
+    return 0;
+}
+
+
